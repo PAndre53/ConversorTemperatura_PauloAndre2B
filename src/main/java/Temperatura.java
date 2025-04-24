553c10723adbbb2d6b6a33f65369a489ae670aee
2024105070035 <2024105070035@aqs-lab-c-28.ifro.local>
2025-04-24 14:42:52 -0400

    Criada a classe Temperatura e iniciada a implementação da classe TemperaturaEspecial

--- src/main/java/Temperatura.java
+++ src/main/java/Temperatura.java
@@ -1,3 +1,29 @@
 public class Temperatura {
+    //ATRIBUTO PRIVADO PARA ARMAZENAR TEMP - CELSIUS
+    private double tempCelsius;
     
+    //CONSTRUTOR - CRIA O OBJETO COM UMA TEMP INICIAL
+    public Temperatura(double tempInicialCelsius){
+        this.tempCelsius = tempInicialCelsius;
+                }
+    
+        //GETTER - PARA LER A TEMP
+        public double getTemperaturaCelsius(){
+            return tempCelsius;
+        }
+        
+        //SETTER - PARA MUDAR A TEMPERATURA
+        public void setTemperaturaCelsius(double mudarTempCelsius){
+            this.tempCelsius = mudarTempCelsius;               
+    }
+   
+        //MÉTODO PARA CONVERTER CELSIUS PARA FAHRENHEITS
+        public double converteParaFahrenheit(){
+            return (tempCelsius*9/5) + 32;
+        }
+        
+        //MÉTODO PARA CONVERTER CELSIUS PARA KELVIN
+        public double converteParaKelvin(){
+            return tempCelsius+273.15;
+        }
 }
